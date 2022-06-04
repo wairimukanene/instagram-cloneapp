540e2127a5b04625e37bff0a33995ad021213b26
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-05 02:49:21 +0300

    commit11-changed database setting in the setting.py

--- social/settings.py
+++ social/settings.py
@@ -78,11 +78,12 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social',
+        'USER': 'wairimu',
+    'PASSWORD':'wairimu12',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
