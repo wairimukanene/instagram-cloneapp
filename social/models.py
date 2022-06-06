4dbb34634f29f0751879b50f10b4dd13e7270f7c
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 05:12:13 +0300

    commit13-import user,post_save andpost_delete in the models.py

--- social/models.py
+++ social/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.utils.text import slugify
+
+
