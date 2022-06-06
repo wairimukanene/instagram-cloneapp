1d21bd5b288e55c88aa0b68e30040dd30ed281a0
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 08:57:51 +0300

    commit17-added cloudinary and also installed

--- sociall/wsgi.py
+++ sociall/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for social project.
+WSGI config for sociall project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sociall.settings')
 
 application = get_wsgi_application()
