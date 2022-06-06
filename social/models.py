abe6fe81fb912ef33b3996488108ea0eafa75659
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 05:20:40 +0300

    commit14-added a function to specify a certain directory to save users files

--- social/models.py
+++ social/models.py
@@ -6,3 +6,14 @@ from django.db.models.signals import post_save,post_delete
 from django.utils.text import slugify
 
 
+# uploading user files to a specific directory
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+
+
+
+
+
+
