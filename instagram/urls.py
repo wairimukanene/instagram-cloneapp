0b09292d6ac34b27921997114a3a432a9662ce7e
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 10:05:37 +0300

    commit28-added django registration folder

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.landing, name='landing'),
-     url(r'^new/post/$', views.new_post, name='new-post'),
-    # url(r'^new/profile/$', views.new_profile, name='new-profile'),
-    # url(r'^profile/(?P<profile_id>\d+)', views.profile, name='profile'),
-    # url(r'^user_profile/(?P<username>\w+)', views.user_profile, name='user_profile'),
-    # url(r'^post/(?P<id>\d+)', views.post_comment, name='comment'),
-    # url(r'^search/', views.search_profile, name='search'),
+    url(r'^new/post/$', views.new_post, name='new-post'),
+    url(r'^new/profile/$', views.new_profile, name='new-profile'),
+    url(r'^profile/(?P<profile_id>\d+)', views.profile, name='profile'),
+    url(r'^user_profile/(?P<username>\w+)', views.user_profile, name='user_profile'),
+    url(r'^post/(?P<id>\d+)', views.post_comment, name='comment'),
+    url(r'^search/', views.search_profile, name='search'),
 ]
--- sociall/urls.py
+++ sociall/urls.py
@@ -22,11 +22,11 @@ from django_registration.backends.one_step.views import RegistrationView
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('',include('instagram.urls')),
-    # url('^accounts/register/',
-    #     RegistrationView.as_view(success_url='/accounts/login'),
-    #     name='django_registration_register'),
-    # url(r'^accounts/', include('django_registration.backends.one_step.urls')),
-    # url(r'^logout/$',views.LogoutView.as_view(), {'next_page': 'settings.LOGOUT_REDIRECT_URL'}),
-    # url(r'^accounts/', include('django.contrib.auth.urls')),
-    # url(r'^tinymce/', include('tinymce.urls'))
+    url('^accounts/register/',
+        RegistrationView.as_view(success_url='/accounts/login'),
+        name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+     url(r'^logout/$',views.LogoutView.as_view(), {'next_page': 'settings.LOGOUT_REDIRECT_URL'}),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^tinymce/', include('tinymce.urls'))
 ]
