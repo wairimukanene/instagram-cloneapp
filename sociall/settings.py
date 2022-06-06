c9f3ce1ad2b40777205aae39128adf9ff0edfe59
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 22:10:27 +0300

    commit32-changed setting.py

--- sociall/settings.py
+++ sociall/settings.py
@@ -138,8 +138,13 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AUTH_PROFILE_MODULE = 'accounts.Profile'
+
+LOGOUT_REDIRECT_URL='/'
 
 LOGIN_REDIRECT_URL='/'
+
+
 
 
 # Default primary key field type
