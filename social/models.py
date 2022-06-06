8185c1e05bd57d495d36de06922abe2ced383829
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-06 07:53:01 +0300

    commit15-added class tag and class meta

--- social/models.py
+++ social/models.py
@@ -4,11 +4,32 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save,post_delete
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 # uploading user files to a specific directory
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
+  
+class Tag(models.Model):
+    title = models.CharField(max_length=75, verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True, default=uuid.uuid1)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def get_absolute_url(self):
+        return reverse('tags', args=[self.slug])
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
 
 
 
